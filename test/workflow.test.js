fe1c822a5962b7e16ee194d70085f6eaedc5809a
sspangsberg <smsj@easv.dk>
2021-04-05 23:31:00 +0200

    Initial commit

---
+++ test/workflow.test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('/First Test Collection', () => {
+
+    it('should test default API welcome route...', (done) => {
+
+        chai.request(server)
+        .get('/welcome')
+        .end((err, res) => {
+            expect(res.status).to.be.equal(200);
+            done();
+        });
+    });
+});
